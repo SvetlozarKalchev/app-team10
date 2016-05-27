9bad9dd933b2f2b98f79ae96616496f7cf3e8d06
mavrovgeorgi <a1404326@unet.univie.ac.at>
2016-05-27 11:36:25 +0200

    finish genre-selection

--- src/client/templates/artist_selection/artist_selection.js
+++ src/client/templates/artist_selection/artist_selection.js
@@ -5,6 +5,6 @@ import './artist_selection.html'
 // This code adds a redirect event to the next button on the page
 Template.artist_selection.events({
   'click .next'(event) {
-    FlowRouter.redirect('/genre-selection')
+    FlowRouter.redirect('/genre-selection');
   }
 });
---
+++ src/client/templates/genre_selection/genre_selection.js
@@ -0,0 +1,10 @@
+import { Template } from 'meteor/templating';
+
+import './genre_selection.html'
+
+Template.genre_selection.events({
+  // This code adds a redirect event to the next button on the page
+  'click .next'(event) {
+    FlowRouter.redirect('/register');
+  }
+});
