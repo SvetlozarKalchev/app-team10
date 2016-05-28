845315499a797112cb882192b6a11574f00ef001
mavrovgeorgi <a1404326@unet.univie.ac.at>
2016-05-28 19:31:15 +0200

    Edit genre-selection next button

--- src/client/templates/genre_selection/genre_selection.js
+++ src/client/templates/genre_selection/genre_selection.js
@@ -1,10 +1,41 @@
 import { Template } from 'meteor/templating';
 
-import './genre_selection.html'
+  var selectedGenres = new Array();
 
-Template.genre_selection.events({
-  // This code adds a redirect event to the next button on the page
-  'click .next'(event) {
-    FlowRouter.redirect('/artist-selection');
+  function saveGenreNames(genreName) {
+    selectedGenres.push(genreName);
+  }
+
+  function sendSelectedGenres() {
+    console.log(selectedGenres);
+  }
+
+  var isArtistBoxClicked = false;
+
+  Template.genre_selection.events({
+    // This code adds a redirect event to the next button on the page
+    'click .next'(event) {
+
+      sendSelectedGenres();
+
+      FlowRouter.redirect('/register');
+
+  },
+
+  'click .genre_box'(event) {
+    // Select only the element that has an event listener attached
+    var clickedElement = event.currentTarget;
+
+    if(!isArtistBoxClicked){
+
+    }
+    var currentClass = clickedElement.getAttribute('class');
+
+    // Add new class to the element with an attached event listener
+    clickedElement.setAttribute('class', currentClass+' highlight')
+
+    // Save selected artist name
+    var genre = clickedElement.firstElementChild.innerHTML;
+    saveGenreNames(artist);
   }
 });
