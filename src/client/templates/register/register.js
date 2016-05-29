308fdcc25ba483d8c1851d6ca515f5bd97b663d7
mavrovgeorgi <a1404326@unet.univie.ac.at>
2016-05-30 01:58:10 +0200

    finish register.js

--- src/client/templates/register/register.js
+++ src/client/templates/register/register.js
@@ -6,12 +6,14 @@ Template.register.events({
 
     var formData = document.getElementsByClassName('reg');
 
+
     var userName = formData[0].value;
     var password = formData[1].value;
     var confirmPassword = formData[2].value;
 
-    if((userName !== " " || userName !== null) && (password !== " " || password !== null) && password === confirmPassword) {
+    if(userName !== '' && password !== '' && password === confirmPassword) {
       FlowRouter.redirect('/recommended-song');
+      
     } else {
       alert('Please, fill all required field')
     }
