36850283f6c41ad2c2b65a1716429be174e63459
mavrovgeorgi <a1404326@unet.univie.ac.at>
2016-05-30 01:43:49 +0200

    work on register.js

--- src/client/templates/register/register.js
+++ src/client/templates/register/register.js
@@ -1,7 +1,20 @@
 import { Template } from 'meteor/templating';
 
-
-
 Template.register.events({
-  
+
+  'click .register-button'(event) {
+
+    var formData = document.getElementsByClassName('reg');
+
+    var userName = formData[0].value;
+    var password = formData[1].value;
+    var confirmPassword = formData[2].value;
+
+    if((userName !== " " || userName !== null) && (password !== " " || password !== null) && password === confirmPassword) {
+      FlowRouter.redirect('/recommended-song');
+    } else {
+      alert('Please, fill all required field')
+    }
+  }
+
 });
