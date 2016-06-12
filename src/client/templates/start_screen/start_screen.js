bb323344530d98e534386782351fd7f81c228ccf
SvetlozarKalchev <svetlozar@kalchev.eu>
2016-06-13 00:08:37 +0200

    Add favorites list

--- src/client/templates/start_screen/start_screen.js
+++ src/client/templates/start_screen/start_screen.js
@@ -1,13 +1,20 @@
 import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
 
+function createFavoritesList() {
+  Session.set('favorites', new Array());
+}
 Template.start_screen.events({
   // This code adds a redirect event to the next button on the page
   'click .next'(event) {
+
+    createFavoritesList();
     FlowRouter.redirect('/genre-selection');
   },
 
   // Add redirect to login page
   'click .redirect_to_login'(event) {
+    createFavoritesList();
     FlowRouter.redirect('/login');
   }
 });
