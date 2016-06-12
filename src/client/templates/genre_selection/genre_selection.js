504c6bee8d86e26026cef31ee4a11790bd6e3f30
mavrovgeorgi <a1404326@unet.univie.ac.at>
2016-06-12 20:58:37 +0200

    edit genre selection

--- src/client/templates/genre_selection/genre_selection.js
+++ src/client/templates/genre_selection/genre_selection.js
@@ -1,16 +1,39 @@
 import { Template } from 'meteor/templating';
 
   var selectedGenres = new Array();
+  var isGenreBoxClicked = false;
+  var selected = false;
 
   function saveGenreNames(genreName) {
-    selectedGenres.push(genreName);
+
+    for(var i = 0; i < selectedGenres.length; i++) {
+      if(genreName === selectedGenres[i])
+      {
+        return false;
+        //console.log("The item is already clicked!")
+        //console.log(genreName + "it is saved.....");
+        //
+      } else {
+        return true;
+      }
+    }
+      if(saveGenreNames(genreName)) {
+        selectedGenres.push(genreName);
+      }
   }
 
   function sendSelectedGenres() {
+    //Here to send the selected items
     console.log(selectedGenres);
   }
 
-  var isGenreBoxClicked = false;
+
+
+  function checkIfClicked() {
+    if(isGenreBoxClicked){
+      isGenreBoxClicked = true;
+    }
+  }
 
   Template.genre_selection.events({
     // This code adds a redirect event to the next button on the page
@@ -19,34 +42,19 @@ import { Template } from 'meteor/templating';
       sendSelectedGenres();
 
       FlowRouter.redirect('/artist-selection');
-
-  },
+    },
 
   'click .genre_box'(event) {
     // Select only the element that has an event listener attached
-    var clickedElement = event.currentTarget;
 
-function checkIfClicked() {
-  if(isGenreBoxClicked){
-    return false;
-    var genre = clickedElement.firstElementChild.innerHTML;
-    saveGenreNames(genre);
-  //  isGenreBoxClicked = true;
-  }
-  else {
-    return true;
-
-  }
-}
-
-
-    var currentClass = clickedElement.getAttribute('class');
+      var clickedElement = event.currentTarget;
+      var genre = clickedElement.firstElementChild.innerHTML;
+      var currentClass = clickedElement.getAttribute('class');
 
     // Add new class to the element with an attached event listener
     clickedElement.setAttribute('class', currentClass+' highlight')
 
     // Save selected artist name
-
-
-  }
+    saveGenreNames(genre);
+    }
 });
