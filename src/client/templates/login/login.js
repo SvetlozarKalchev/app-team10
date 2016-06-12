dd15ac384ae7857e81c2af2d9947e3abe303b25f
okunska <okunska@yahoo.com>
2016-06-13 00:31:11 +0200

    Update Login & Registration

--- src/client/templates/login/login.js
+++ src/client/templates/login/login.js
@@ -5,5 +5,51 @@ Template.login.events({
   'click .register'(event) {
     FlowRouter.redirect('/register');
   }
+});
 
+Template.login.events({
+  'submit form' : function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        var currentUser = Meteor.userId();
+        if(currentUser){
+            alert(currentUser);
+        }else{
+
+            Meteor.loginWithPassword(email, password, function(error){
+                if(error){
+                    console.log("Error");
+                    alert("Invalid Email or Password");
+                }else{
+                    console.log("Login!");
+                }
+            });
+        }
+
+      var currentUser = Meteor.userId();
+      if(currentUser){
+          FlowRouter.redirect('/recommended-song');
+      }else{
+          console.log("Invalid Email or Password");
+      }
+    }
+  });
+
+
+
+Template.login.events({
+    'click .logout': function(event){
+     event.preventDefault();
+
+    Meteor.logout();
+  }
+});
+
+
+Template.login.events({
+  // This code adds a redirect event to the next button on the page
+  'click .next'(event) {
+    FlowRouter.redirect('/recommended-song');
+  }
 });
--- src/client/templates/register/register.js
+++ src/client/templates/register/register.js
@@ -7,3 +7,20 @@ Template.register.events({
     FlowRouter.redirect('/login');
   }
 });
+Template.register.events({
+  'submit form' : function(event){
+   event.preventDefault();
+    var email = $('[name=usernameReg]').val();
+    var password = $('[name=passwordReg]').val();
+    var confPassword = $('[name=passwordRegConf]').val();
+    if(password!=confPassword){
+      alert('Invalid Password! Please, Confirm password');
+    }else{
+    Accounts.createUser({
+      email: email,
+      password: password
+    });
+    FlowRouter.redirect('/recommended-song');
+  }
+  }
+});
